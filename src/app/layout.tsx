75b4ec355bd20b90e3a890b185b696fce83e19f6
Mateus Abreu <mateusabreucn@gmail.com>
2023-08-17 14:56:48 -0300

    :truck: moving the Header.tsx file to another dir

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import Provider from "./Provider";
-import Header from "../components/Header";
+import Header from "../components/sections/Header";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -13,6 +13,7 @@ export const metadata: Metadata = {
   title: "Rotina Monetária",
   description:
     "Um planejador financeiro de uso pessoal para a gestão de suas finanças",
+  icons: "./favicon.svg",
 };
 
 export default function RootLayout({
@@ -23,9 +24,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Provider>
-        <body className={poppins.className}>
-          <Header />
-          {children}
+        <body className={`${poppins.className} container mx-auto flex-col`}>
+          <div className="min-h-screen flex-grow">
+            <Header />
+            {children}
+          </div>
         </body>
       </Provider>
     </html>
