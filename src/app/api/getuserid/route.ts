b9837fef508b21af4b5a6cc9fba88887f402a22f
Mateus Abreu <mateusabreucn@gmail.com>
2023-08-17 14:57:22 -0300

    :sparkles: feat: new route to get the user ID

---
+++ src/app/api/getuserid/route.ts
@@ -0,0 +1,24 @@
+import { prisma } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const email = req.nextUrl.searchParams.get("email");
+
+  if (!email) {
+    return NextResponse.json({ error: "Email is required", status: 400 });
+  }
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: { email: email },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "User not found", status: 404 });
+    }
+
+    return NextResponse.json({ userId: user.id });
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to fetch user ID", status: 500 });
+  }
+}
