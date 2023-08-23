9dde98fe597a8abb750ce98a20537a3599d3e78f
Mateus Abreu <mateusabreucn@gmail.com>
2023-08-23 18:41:22 -0300

    :bug: fix: pass the right id to database

--- src/app/api/finances/[id]/route.ts
+++ src/app/api/finances/[id]/route.ts
@@ -6,25 +6,22 @@ export async function PUT(
   req: Request,
   { params }: { params: { id: string } }
 ) {
-  const id = params.id;
-  const transactionId = await prisma.financeTransaction.findFirst({
-    where: { id },
-  });
-
-  if (!id || typeof id !== "string" || !transactionId) {
-    return NextResponse.json({ error: "Invalid ID provided", status: 500 });
-  }
-
-  const financeInput: FinanceInput = await req.json();
-
-  if (financeInput.date) {
-    const dateObject = new Date(financeInput.date);
-    financeInput.date = dateObject;
-  }
-
   try {
+    const id = params.id;
+
+    if (!id) {
+      return NextResponse.json({ error: "Invalid ID provided", status: 500 });
+    }
+
+    const financeInput: FinanceInput = await req.json();
+
+    if (financeInput.date) {
+      const dateObject = new Date(financeInput.date);
+      financeInput.date = dateObject;
+    }
+
     const updatedFinance = await prisma.financeTransaction.update({
-      where: transactionId,
+      where: { id: id },
       data: financeInput,
     });
     return NextResponse.json(updatedFinance);
@@ -40,18 +37,15 @@ export async function DELETE(
   req: Request,
   { params }: { params: { id: string } }
 ) {
-  const id = params.id;
-  const transactionId = await prisma.financeTransaction.findFirst({
-    where: { id },
-  });
-
-  if (!id || typeof id !== "string" || !transactionId) {
-    return NextResponse.json({ error: "Invalid ID provided", status: 500 });
-  }
-
   try {
+    const id = params.id;
+
+    if (!id) {
+      return NextResponse.json({ error: "Invalid ID provided", status: 500 });
+    }
+
     await prisma.financeTransaction.delete({
-      where: transactionId,
+      where: { id: id },
     });
     return NextResponse.json({ message: "Finance entry deleted successfuly" });
   } catch (error) {
