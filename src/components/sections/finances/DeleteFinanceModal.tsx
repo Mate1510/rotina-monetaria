8f79ea5d0057d42463fdba249b27fbf6fa2da77b
Mateus Abreu <mateusabreucn@gmail.com>
2023-08-23 18:35:25 -0300

    :truck: chore: renaming the file

---
+++ src/components/sections/finances/DeleteFinanceModal.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from "react";
+import ModalComponent from "@/components/sections/Modal";
+import Button from "@/components/components/Button";
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+};
+
+const DeleteFinanceModal: React.FC<Props> = ({
+  isOpen,
+  onClose,
+  onConfirm,
+}) => {
+  return (
+    <ModalComponent
+      isOpen={isOpen}
+      onClose={onClose}
+      modalTitle="Confirmar Exclusão"
+      actionButton={
+        <Button
+          onClick={onConfirm}
+          className="bg-primaryOrange p-2 rounded-lg text-white font-medium text-lg"
+        >
+          Confirmar
+        </Button>
+      }
+    >
+      <p className="text-lg text-constrastBlack font-medium text-center">
+        Você tem certeza que irá deletar esta finança?
+      </p>
+    </ModalComponent>
+  );
+};
+
+export default DeleteFinanceModal;
