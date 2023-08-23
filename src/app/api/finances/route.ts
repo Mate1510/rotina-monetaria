7386d4bc947ff66d83d2c790a096b7a31bce8875
Mateus Abreu <mateusabreucn@gmail.com>
2023-08-23 18:33:13 -0300

    :bug: fix: receiving the userId and fixing type issues

--- src/app/api/finances/route.ts
+++ src/app/api/finances/route.ts
@@ -5,23 +5,35 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   const month = Number(req.nextUrl.searchParams.get("month"));
   const year = Number(req.nextUrl.searchParams.get("year"));
+  const userId = req.nextUrl.searchParams.get("userid");
 
   if (!month || !year) {
-    return NextResponse.json({ error: "Month and year are required" });
+    return NextResponse.json({
+      error: "Month and year are required",
+      status: 500,
+    });
+  }
+
+  if (!userId) {
+    return NextResponse.json({ error: "USer not authenticated", status: 500 });
   }
 
   const startDate = new Date(year, month - 1, 1);
   const endDate = new Date(year, month, 0);
 
   try {
-    const finances: Finance[] = await prisma.financeTransaction.findMany({
+    const finances: Finance[] = (await prisma.financeTransaction.findMany({
       where: {
+        userId: userId,
         date: {
           gte: startDate,
           lte: endDate,
         },
       },
-    });
+      orderBy: {
+        date: "asc",
+      },
+    })) as Finance[];
     return NextResponse.json(finances);
   } catch (error) {
     return NextResponse.json({ error: "Failed to fetch finances" });
@@ -41,9 +53,10 @@ export async function POST(req: Request) {
   }
 
   try {
-    const newFinance: Finance = await prisma.financeTransaction.create({
+    const newFinance: Finance = (await prisma.financeTransaction.create({
       data: financeInput,
-    });
+    })) as Finance;
+
     return NextResponse.json(newFinance, {
       status: 200,
     });
