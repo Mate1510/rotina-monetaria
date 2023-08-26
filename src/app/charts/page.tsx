2caa5eec94bc82338b47f4ebf07d45639b25105e
Mateus Abreu <mateusabreucn@gmail.com>
2023-08-26 11:52:15 -0300

    :sparkles: feat: creating the charts

---
+++ src/app/charts/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RoundedCharts from "@/components/sections/charts/RoundedCharts";
+
+const ChartsPage = () => {
+  return (
+    <div className="container flex min-h-screen flex-col items-center p-5 gap-12">
+      <RoundedCharts />
+    </div>
+  );
+};
+
+export default ChartsPage;
---
+++ src/components/sections/charts/RoundedCharts.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import { Category } from "@/categories";
+import { Finance } from "@/finance";
+import { Doughnut, Pie } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const RoundedCharts = () => {
+  const [chartData, setChartData] = useState({});
+  const [chartType, setChartType] = useState("doughnut");
+  const [month, setMonth] = useState(new Date().getMonth() + 1);
+  const [year, setYear] = useState(new Date().getFullYear());
+
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (!session) {
+      console.error("User not authenticated.");
+      return;
+    }
+
+    const fetchData = async () => {
+      try {
+        const userIdResponse = await axios.get(
+          `/api/getuserid?email=${session.user?.email}`
+        );
+        const userId = await userIdResponse.data.userId;
+
+        const categoriesResponse = await axios.get("/api/categories", {
+          params: {
+            userid: userId,
+          },
+        });
+        const categoriesData: Category[] = await categoriesResponse.data;
+
+        const financesResponse = await axios.get(`/api/finances`, {
+          params: { month, year, userid: userId },
+        });
+        const financesData: Finance[] = await financesResponse.data;
+
+        const data = {
+          labels: categoriesData.map((category) => category.name),
+          datasets: [
+            {
+              data: financesData.map((finance) => {finance.value}),
+              backgroundColor: categoriesData.map((category) => category.color),
+            },
+          ],
+        };
+        setChartData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [session]);
+
+  const handlePrevMonth = () => {
+    if (month === 1) {
+      setMonth(12);
+      setYear(year - 1);
+    } else {
+      setMonth(month - 1);
+    }
+  };
+
+  const handleNextMonth = () => {
+    if (month === 12) {
+      setMonth(1);
+      setYear(year + 1);
+    } else {
+      setMonth(month + 1);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handlePrevMonth}>Previous Month</button>
+      <span>{`${month}/${year}`}</span>
+      <button onClick={handleNextMonth}>Next Month</button>
+      <Doughnut data={chartData} />
+    </div>
+  );
+};
+
+export default RoundedCharts;
