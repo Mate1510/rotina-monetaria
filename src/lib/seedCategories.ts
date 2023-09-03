441c9583d83fda7b727c2dd9d525678c233fc8ab
Mateus Abreu <mateusabreucn@gmail.com>
2023-09-03 15:29:11 -0300

    :recycle: refactor: changing the way to seed categories

---
+++ src/lib/seedCategories.ts
@@ -0,0 +1,56 @@
+import { prisma } from "@/lib/db";
+
+export async function seedCategories(userId: string) {
+  const categories = await prisma.category.createMany({
+    data: [
+      {
+        name: "Salário",
+        transactionType: "INCOME",
+        color: "GREEN",
+        userId: userId,
+      },
+      {
+        name: "Investimentos",
+        transactionType: "INCOME",
+        color: "ORANGE",
+        userId: userId,
+      },
+      {
+        name: "Bônus",
+        transactionType: "INCOME",
+        color: "AQUA",
+        userId: userId,
+      },
+      {
+        name: "Casa",
+        transactionType: "EXPENSE",
+        color: "YELLOW",
+        userId: userId,
+      },
+      {
+        name: "Lazer",
+        transactionType: "EXPENSE",
+        color: "PINK",
+        userId: userId,
+      },
+      {
+        name: "Alimentação",
+        transactionType: "EXPENSE",
+        color: "RED",
+        userId: userId,
+      },
+      {
+        name: "Transporte",
+        transactionType: "EXPENSE",
+        color: "BLACK",
+        userId: userId,
+      },
+      {
+        name: "Plano de Assinatura",
+        transactionType: "EXPENSE",
+        color: "BLUE",
+        userId: userId,
+      },
+    ],
+  });
+}
