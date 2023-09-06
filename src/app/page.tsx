fb4296c8a84d06f1f65c053e553f7ea26ad09409
Mateus Abreu <mateusabreucn@gmail.com>
2023-09-06 19:47:14 -0300

    :ok_hand: style: removing unecessary items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import Finances from "@/components/sections/finances/InsertFinances";
 import CardSection from "@/components/sections/homepage/CardSection";
 import FinanceTable from "@/components/sections/homepage/FinanceTable";
 
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="container flex min-h-screen flex-col items-center p-5 gap-12">
