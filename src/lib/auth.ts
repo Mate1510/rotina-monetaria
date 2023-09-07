95d08ceb11ee2742dccdec6b221d0914b35d7694
Mateus Abreu <mateusabreucn@gmail.com>
2023-09-07 19:24:09 -0300

    :sparkles: feat: adding userId callback to session

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,76 +6,83 @@ import { prisma } from "@/lib/db";
 import bcrypt from "bcrypt";
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma as any),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    CredentialProvider({
-      name: "credentials",
-      credentials: {
-        email: { label: "E-mail", type: "text", placeholder: "E-mail" },
-        password: {
-          label: "Password",
-          type: "Password",
-          placeholder: "Senha",
+    adapter: PrismaAdapter(prisma as any),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+        CredentialProvider({
+            name: "credentials",
+            credentials: {
+                email: { label: "E-mail", type: "text", placeholder: "E-mail" },
+                password: {
+                    label: "Password",
+                    type: "Password",
+                    placeholder: "Senha",
+                },
+                name: { label: "Name", type: "text", placeholder: "Nome" },
+            },
+            async authorize(credentials, req): Promise<any> {
+                if (!credentials?.email || !credentials.password) {
+                    throw new Error("No credentials passed");
+                }
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email,
+                    },
+                });
+
+                if (!user || !user.password) {
+                    throw new Error("No user found with this credentials");
+                }
+
+                if (user.status == "INACTIVE") {
+                    throw new Error("The user is inactive!");
+                }
+
+                const isPasswordValid = await bcrypt.compare(
+                    credentials.password,
+                    user.password
+                );
+
+                if (!isPasswordValid) {
+                    throw new Error("Invalid password");
+                }
+
+                return user;
+            },
+        }),
+    ],
+    callbacks: {
+        async jwt({ token, user, session }) {
+            console.log("Token: ", { session, token, user });
+            if (user) {
+                return {
+                    ...token,
+                    userId: user.id,
+                };
+            }
+            return token;
         },
-        name: { label: "Name", type: "text", placeholder: "Nome" },
-      },
-      async authorize(credentials, req): Promise<any> {
-        if (!credentials?.email || !credentials.password) {
-          throw new Error("No credentials passed");
-        }
-
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials.email,
-          },
-        });
-
-        if (!user || !user.password) {
-          throw new Error("No user found with this credentials");
-        }
-
-        if (user.status == "INACTIVE") {
-          throw new Error("The user is inactive!");
-        }
-
-        const isPasswordValid = await bcrypt.compare(
-          credentials.password,
-          user.password
-        );
-
-        if (!isPasswordValid) {
-          throw new Error("Invalid password");
-        }
-
-        return user;
-      },
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
-  // callbacks: {
-  //   async jwt({ token, account, user }) {
-  //     if (account) {
-  //       token.accessToken = account.access_token;
-  //       token.id = user?.id;
-  //     }
-  //     return token;
-  //   },
-  //   async session({ session, token, user }) {
-  //     session.accessToken = token.accessToken;
-  //     session.user.id = token.id;
-
-  //     return session;
-  //   },
-  // },
-  secret: process.env.SECRET,
-  debug: process.env.NODE_ENV === "development",
-  pages: {
-    signIn: "/login",
-  },
+        async session({ session, token, user }) {
+            console.log("Session: ", { session, token, user });
+            return {
+                ...session,
+                user: {
+                    ...session.user,
+                    userId: token.userId,
+                },
+            };
+        },
+    },
+    secret: process.env.SECRET,
+    session: {
+        strategy: "jwt",
+    },
+    debug: process.env.NODE_ENV === "development",
+    pages: {
+        signIn: "/login",
+    },
 };
---
+++ types/session.d.ts
@@ -0,0 +1,12 @@
+import "next-auth";
+
+declare module "next-auth" {
+    interface Session {
+        user: {
+            name?: string | null;
+            email?: string | null;
+            image?: string | null;
+            userId?: string | null;
+        };
+    }
+}
