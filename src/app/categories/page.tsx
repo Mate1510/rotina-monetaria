2bdcb1c840fd57e05ade77c0c6ac71b708f420d4
Mateus Abreu <mateusabreucn@gmail.com>
2023-09-13 14:02:35 -0300

    :iphone: style: responsiveness

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -4,7 +4,7 @@ import CategoriesTableExtended from "@/components/sections/categories/Categories
 
 const CategoriesPage = () => {
   return (
-    <div className="container flex min-h-screen flex-col items-center p-5 gap-12">
+    <div className="container flex min-h-screen flex-col items-center p-5 gap-12 lg:max-w-screen-xl lg:mx-auto">
       <InsertCategories />
       <CategoriesTableExtended />
     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <Provider>
         <body className={`${poppins.className} mx-auto flex-col `}>
           <Header />
-          <div className="container min-h-screen flex-grow 2xl:max-w-screen-2xl lg:max-w-screen-2xl lg:mx-auto">
+          <div className="container min-h-screen flex-grow lg:max-w-screen-2xl lg:mx-auto my-10">
             {children}
           </div>
           <Footer />
