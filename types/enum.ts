3496d8f13749674d1dee9fa49c94064e7a5326a9
Mateus Abreu <mateusabreucn@gmail.com>
2023-09-13 13:53:38 -0300

    :lipstick: stle: changing colors

--- types/enum.ts
+++ types/enum.ts
@@ -1,13 +1,13 @@
 export enum Color {
-  RED = "#ff0000",
-  ORANGE = "#ee4b1f",
-  PURPLE = "#a020f0",
+  RED = "#ff4069",
+  ORANGE = "#ff9021",
+  PURPLE = "#9966ff",
   PINK = "#ff00ff",
-  BLUE = "#0000ff",
-  AQUA = "#00ffff",
-  GREEN = "#00ff00",
-  YELLOW = "#ffff00",
-  BLACK = "#0a0a0a",
+  BLUE = "#059bff",
+  AQUA = "#04ecf0",
+  GREEN = "#4bc0c0",
+  YELLOW = "#ffc234",
+  BLACK = "#252525",
   WHITE = "#ffffff",
 }
 
