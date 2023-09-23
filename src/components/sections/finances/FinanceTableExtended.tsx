739e79dacd97b4e3cb266becc8e515842c0cee83
Mateus Abreu <mateusabreucn@gmail.com>
2023-09-23 15:30:58 -0300

    :recycle: refactor: add fetchFinances in a function

--- src/components/sections/finances/FinanceTableExtended.tsx
+++ src/components/sections/finances/FinanceTableExtended.tsx
@@ -9,259 +9,256 @@ import { Category } from "@/categories";
 import { MdTrendingUp, MdTrendingDown, MdDelete, MdEdit } from "react-icons/md";
 import EditFinanceModal from "./EditFinanceModal";
 import DeleteFinanceModal from "./DeleteFinanceModal";
+import useFetchFinances from "@/data/useFetchFinances";
+import useFetchCategories from "@/data/useFetchCategories";
 
 const FinanceTable = () => {
-  const [finances, setFinances] = useState<Finance[]>([]);
-  const [month, setMonth] = useState(new Date().getMonth() + 1);
-  const [year, setYear] = useState(new Date().getFullYear());
-  const [categories, setCategories] = useState<Category[]>([]);
-  const [financeToEdit, setFinanceToEdit] = useState<Finance | null>(null);
-  const [financeToDelete, setFinanceToDelete] = useState<Finance | null>(null);
+    const [finances, setFinances] = useState<Finance[]>([]);
+    const [categories, setCategories] = useState<Category[]>([]);
+    const [financeToEdit, setFinanceToEdit] = useState<Finance | null>(null);
+    const [financeToDelete, setFinanceToDelete] = useState<Finance | null>(
+        null
+    );
+    const [month, setMonth] = useState(new Date().getMonth() + 1);
+    const [year, setYear] = useState(new Date().getFullYear());
 
-  const { data: session } = useSession();
+    const { data: dataFinances } = useFetchFinances(month, year);
+    const { data: dataCategories } = useFetchCategories();
 
-  const monthNames = [
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro",
-  ];
+    const { data: session } = useSession();
 
-  useEffect(() => {
-    if (!session) {
-      console.error("User not authenticated.");
-      return;
-    }
+    const monthNames = [
+        "Janeiro",
+        "Fevereiro",
+        "Março",
+        "Abril",
+        "Maio",
+        "Junho",
+        "Julho",
+        "Agosto",
+        "Setembro",
+        "Outubro",
+        "Novembro",
+        "Dezembro",
+    ];
 
-    const fetchFinances = async () => {
-      try {
-        const userIdResponse = await axios.get(
-          `/api/get-user-info/user-id?email=${session.user?.email}`
-        );
-        const userId = await userIdResponse.data.userId;
+    useEffect(() => {
+        if (!session) {
+            console.error("User not authenticated.");
+            return;
+        }
 
-        const financesResponse = await axios.get(`/api/finances`, {
-          params: { month, year, userid: userId },
-        });
-        const financesData: Finance[] = await financesResponse.data;
-        setFinances(financesData);
-      } catch (error) {
-        console.error("Failed to fetch finances:", error);
-      }
+        if (Array.isArray(dataFinances) && Array.isArray(dataCategories)) {
+            setFinances(dataFinances);
+            setCategories(dataCategories);
+        }
+    }, [session, month, year, dataFinances, dataCategories]);
+
+    const handlePrevMonth = () => {
+        setMonth((prevMonth) => (prevMonth === 1 ? 12 : prevMonth - 1));
+        setYear((prevYear) => (month === 1 ? prevYear - 1 : prevYear));
     };
 
-    const fetchCategories = async () => {
-      try {
-        const userIdResponse = await axios.get(
-          `/api/get-user-info/user-id?email=${session.user?.email}`
-        );
-        const userId = await userIdResponse.data.userId;
-
-        const categoriesResponse = await axios.get(
-          `/api/categories?userid=${userId}`
-        );
-        const categoriesData: Category[] = await categoriesResponse.data;
-        setCategories(categoriesData);
-      } catch (error) {
-        console.error("Failed to fetch categories:", error);
-      }
+    const handleNextMonth = () => {
+        setMonth((prevMonth) => (prevMonth === 12 ? 1 : prevMonth + 1));
+        setYear((prevYear) => (month === 12 ? prevYear + 1 : prevYear));
     };
 
-    fetchFinances();
-    fetchCategories();
-  }, [session, month, year]);
+    const handleEditClick = (finance: Finance) => {
+        setFinanceToEdit(finance);
+    };
 
-  const handlePrevMonth = () => {
-    setMonth((prevMonth) => (prevMonth === 1 ? 12 : prevMonth - 1));
-    setYear((prevYear) => (month === 1 ? prevYear - 1 : prevYear));
-  };
+    const handleSave = async (updatedFinance: Finance) => {
+        try {
+            const response = await axios.put(
+                `/api/finances/${updatedFinance.id}`,
+                {
+                    name: updatedFinance.name,
+                    value: updatedFinance.value,
+                    categoryId: updatedFinance.categoryId,
+                    date: updatedFinance.date,
+                    type: updatedFinance.type,
+                }
+            );
 
-  const handleNextMonth = () => {
-    setMonth((prevMonth) => (prevMonth === 12 ? 1 : prevMonth + 1));
-    setYear((prevYear) => (month === 12 ? prevYear + 1 : prevYear));
-  };
+            if (!response) {
+                throw new Error("Failed to update finance data.");
+            }
 
-  const handleEditClick = (finance: Finance) => {
-    setFinanceToEdit(finance);
-  };
+            const updatedFinances = finances.map((finance) =>
+                finance.id === updatedFinance.id ? updatedFinance : finance
+            );
+            setFinances(updatedFinances);
+        } catch (error) {
+            console.error(error);
+        }
 
-  const handleSave = async (updatedFinance: Finance) => {
-    try {
-      const response = await axios.put(`/api/finances/${updatedFinance.id}`, {
-        name: updatedFinance.name,
-        value: updatedFinance.value,
-        categoryId: updatedFinance.categoryId,
-        date: updatedFinance.date,
-        type: updatedFinance.type,
-      });
+        setFinanceToEdit(null);
+    };
 
-      if (!response) {
-        throw new Error("Failed to update finance data.");
-      }
+    const handleDeleteClick = (finance: Finance) => {
+        setFinanceToDelete(finance);
+    };
 
-      const updatedFinances = finances.map((finance) =>
-        finance.id === updatedFinance.id ? updatedFinance : finance
-      );
-      setFinances(updatedFinances);
-    } catch (error) {
-      console.error(error);
-    }
+    const handleConfirmDelete = async () => {
+        if (!financeToDelete) return;
 
-    setFinanceToEdit(null);
-  };
+        try {
+            const response = await axios.delete(
+                `/api/finances/${financeToDelete.id}`
+            );
 
-  const handleDeleteClick = (finance: Finance) => {
-    setFinanceToDelete(finance);
-  };
+            if (!response) {
+                throw new Error("Failed to delete finance entry.");
+            }
 
-  const handleConfirmDelete = async () => {
-    if (!financeToDelete) return;
+            const updatedFinances = finances.filter(
+                (finance) => finance.id !== financeToDelete.id
+            );
+            setFinances(updatedFinances);
+        } catch (error) {
+            console.error(error);
+        }
+        setFinanceToDelete(null);
+    };
 
-    try {
-      const response = await axios.delete(
-        `/api/finances/${financeToDelete.id}`
-      );
+    const getCategoryName = (categoryId: string): string => {
+        const category = categories.find(
+            (category) => category.id === categoryId
+        );
+        return category ? category.name : "Unknown";
+    };
 
-      if (!response) {
-        throw new Error("Failed to delete finance entry.");
-      }
+    return (
+        <div className="min-w-full w-full">
+            <div className="flex items-center justify-center mb-3">
+                <MdOutlineNavigateBefore
+                    onClick={handlePrevMonth}
+                    className="text-primaryOrange cursor-pointer"
+                    size={38}
+                />
+                <h3 className="text-primaryOrange font-semibold text-2xl">{`${
+                    monthNames[month - 1]
+                }`}</h3>
+                <MdOutlineNavigateNext
+                    onClick={handleNextMonth}
+                    className="text-primaryOrange cursor-pointer"
+                    size={38}
+                />
+            </div>
 
-      const updatedFinances = finances.filter(
-        (finance) => finance.id !== financeToDelete.id
-      );
-      setFinances(updatedFinances);
-    } catch (error) {
-      console.error(error);
-    }
+            <div className="min-w-full overflow-x-auto rounded-2xl bg-primaryOrange p-0.5 no-scrollbar">
+                <table className="min-w-full bg-white overflow-hidden rounded-2xl">
+                    <thead className="bg-primaryOrange text-left text-white font-medium text-base">
+                        <tr>
+                            <th className="py-2 px-4 border-b font-medium">
+                                Título
+                            </th>
+                            <th className="py-2 px-4 border-b font-medium">
+                                Categoria
+                            </th>
+                            <th className="py-2 px-4 border-b font-medium">
+                                Data
+                            </th>
+                            <th className="py-2 px-4 border-b font-medium">
+                                Tipo
+                            </th>
+                            <th className="py-2 px-4 border-b font-medium">
+                                Valor
+                            </th>
+                            <th className="py-2 px-4 border-b font-medium text-center">
+                                Ações
+                            </th>
+                        </tr>
+                    </thead>
 
-    setFinanceToDelete(null);
-  };
+                    <tbody className="font-medium">
+                        {finances.map((finance) => (
+                            <tr key={finance.id}>
+                                <td className="py-2 px-4 border-b">
+                                    {finance.name}
+                                </td>
+                                <td className="py-2 px-4 border-b">
+                                    {getCategoryName(finance.categoryId)}
+                                </td>
+                                <td className="py-2 px-4 border-b">
+                                    {new Date(
+                                        finance.date
+                                    ).toLocaleDateString()}
+                                </td>
+                                {finance.type === "INCOME" ? (
+                                    <td className="py-2 px-4 border-b text-green-500">
+                                        <MdTrendingUp size={38} />
+                                    </td>
+                                ) : (
+                                    <td className="py-2 px-4 border-b text-red-500">
+                                        <MdTrendingDown size={38} />
+                                    </td>
+                                )}
+                                <td
+                                    className={`py-2 px-4 border-b ${
+                                        finance.type === "INCOME"
+                                            ? "text-green-500"
+                                            : "text-red-500"
+                                    }`}
+                                >
+                                    {new Intl.NumberFormat("pt-BR", {
+                                        style: "currency",
+                                        currency: "BRL",
+                                    }).format(Number(finance.value))}
+                                </td>
+                                <td className="py-2 px-4 border-b">
+                                    <div className="flex justify-around items-center">
+                                        <MdEdit
+                                            className="text-primaryOrange cursor-pointer"
+                                            onClick={() =>
+                                                handleEditClick(finance)
+                                            }
+                                        />
+                                        <MdDelete
+                                            className="text-primaryOrange cursor-pointer"
+                                            onClick={() =>
+                                                handleDeleteClick(finance)
+                                            }
+                                        />
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
 
-  const getCategoryName = (categoryId: string): string => {
-    const category = categories.find((category) => category.id === categoryId);
-    return category ? category.name : "Unknown";
-  };
+                        {finances.length === 0 && (
+                            <tr>
+                                <td
+                                    colSpan={6}
+                                    className="text-center text-constrastBlack font-medium text-lg"
+                                >
+                                    Suas finanças não chegaram aqui ainda...
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
 
-  return (
-    <div className="min-w-full w-full">
-      <div className="flex items-center justify-center mb-3">
-        <MdOutlineNavigateBefore
-          onClick={handlePrevMonth}
-          className="text-primaryOrange cursor-pointer"
-          size={38}
-        />
-        <h3 className="text-primaryOrange font-semibold text-2xl">{`${
-          monthNames[month - 1]
-        }`}</h3>
-        <MdOutlineNavigateNext
-          onClick={handleNextMonth}
-          className="text-primaryOrange cursor-pointer"
-          size={38}
-        />
-      </div>
-
-      <div className="min-w-full overflow-x-auto rounded-2xl bg-primaryOrange p-0.5 no-scrollbar">
-        <table className="min-w-full bg-white overflow-hidden rounded-2xl">
-          <thead className="bg-primaryOrange text-left text-white font-medium text-base">
-            <tr>
-              <th className="py-2 px-4 border-b font-medium">Título</th>
-              <th className="py-2 px-4 border-b font-medium">Categoria</th>
-              <th className="py-2 px-4 border-b font-medium">Data</th>
-              <th className="py-2 px-4 border-b font-medium">Tipo</th>
-              <th className="py-2 px-4 border-b font-medium">Valor</th>
-              <th className="py-2 px-4 border-b font-medium text-center">
-                Ações
-              </th>
-            </tr>
-          </thead>
-
-          <tbody className="font-medium">
-            {finances.map((finance) => (
-              <tr key={finance.id}>
-                <td className="py-2 px-4 border-b">{finance.name}</td>
-                <td className="py-2 px-4 border-b">
-                  {getCategoryName(finance.categoryId)}
-                </td>
-                <td className="py-2 px-4 border-b">
-                  {new Date(finance.date).toLocaleDateString()}
-                </td>
-                {finance.type === "INCOME" ? (
-                  <td className="py-2 px-4 border-b text-green-500">
-                    <MdTrendingUp size={38} />
-                  </td>
-                ) : (
-                  <td className="py-2 px-4 border-b text-red-500">
-                    <MdTrendingDown size={38} />
-                  </td>
+                {financeToEdit && (
+                    <EditFinanceModal
+                        isOpen={!!financeToEdit}
+                        onClose={() => setFinanceToEdit(null)}
+                        finance={financeToEdit}
+                        onSave={handleSave}
+                        categories={categories}
+                    />
                 )}
-                <td
-                  className={`py-2 px-4 border-b ${
-                    finance.type === "INCOME"
-                      ? "text-green-500"
-                      : "text-red-500"
-                  }`}
-                >
-                  {new Intl.NumberFormat("pt-BR", {
-                    style: "currency",
-                    currency: "BRL",
-                  }).format(Number(finance.value))}
-                </td>
-                <td className="py-2 px-4 border-b">
-                  <div className="flex justify-around items-center">
-                    <MdEdit
-                      className="text-primaryOrange cursor-pointer"
-                      onClick={() => handleEditClick(finance)}
+
+                {financeToDelete && (
+                    <DeleteFinanceModal
+                        isOpen={!!financeToDelete}
+                        onClose={() => setFinanceToDelete(null)}
+                        onConfirm={handleConfirmDelete}
                     />
-                    <MdDelete
-                      className="text-primaryOrange cursor-pointer"
-                      onClick={() => handleDeleteClick(finance)}
-                    />
-                  </div>
-                </td>
-              </tr>
-            ))}
-
-            {finances.length === 0 && (
-              <tr>
-                <td
-                  colSpan={6}
-                  className="text-center text-constrastBlack font-medium text-lg"
-                >
-                  Suas finanças não chegaram aqui ainda...
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-
-        {financeToEdit && (
-          <EditFinanceModal
-            isOpen={!!financeToEdit}
-            onClose={() => setFinanceToEdit(null)}
-            finance={financeToEdit}
-            onSave={handleSave}
-            categories={categories}
-          />
-        )}
-
-        {financeToDelete && (
-          <DeleteFinanceModal
-            isOpen={!!financeToDelete}
-            onClose={() => setFinanceToDelete(null)}
-            onConfirm={handleConfirmDelete}
-          />
-        )}
-      </div>
-    </div>
-  );
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default FinanceTable;
--- src/components/sections/homepage/FinanceTable.tsx
+++ src/components/sections/homepage/FinanceTable.tsx
@@ -5,138 +5,124 @@ import axios from "axios";
 import { useSession } from "next-auth/react";
 import { TransactionType } from "@/enum";
 import { MdOutlineNavigateNext, MdOutlineNavigateBefore } from "react-icons/md";
-
-type Finance = {
-  id: number;
-  name: string;
-  value: number;
-  type: TransactionType;
-};
+import useFetchFinances from "@/data/useFetchFinances";
+import { Finance } from "@/finance";
 
 const FinanceTable = () => {
-  const [finances, setFinances] = useState<Finance[]>([]);
-  const [month, setMonth] = useState(new Date().getMonth() + 1);
-  const [year, setYear] = useState(new Date().getFullYear());
+    const [finances, setFinances] = useState<Finance[]>([]);
+    const [month, setMonth] = useState(new Date().getMonth() + 1);
+    const [year, setYear] = useState(new Date().getFullYear());
 
-  const { data: session } = useSession();
+    const { data: dataFinances } = useFetchFinances(month, year);
 
-  const monthNames = [
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro",
-  ];
+    const { data: session } = useSession();
 
-  useEffect(() => {
-    if (!session) {
-      console.error("User not authenticated.");
-      return;
-    }
+    const monthNames = [
+        "Janeiro",
+        "Fevereiro",
+        "Março",
+        "Abril",
+        "Maio",
+        "Junho",
+        "Julho",
+        "Agosto",
+        "Setembro",
+        "Outubro",
+        "Novembro",
+        "Dezembro",
+    ];
 
-    const fetchFinances = async () => {
-      try {
-        const userId = session?.user?.userId;
+    useEffect(() => {
+        if (!session) {
+            console.error("User not authenticated.");
+            return;
+        }
 
-        const financesResponse = await axios.get(`/api/finances`, {
-          params: { month, year, userid: userId },
-        });
-        console.log("API Response:", financesResponse.data);
-        const financesData: Finance[] = await financesResponse.data;
-        setFinances(financesData);
-      } catch (error) {
-        console.error("Failed to fetch finances:", error);
-      }
+        if (Array.isArray(dataFinances)) {
+            setFinances(dataFinances);
+        }
+    }, [session, month, year, dataFinances]);
+
+    const handlePrevMonth = () => {
+        setMonth((prevMonth) => (prevMonth === 1 ? 12 : prevMonth - 1));
+        setYear((prevYear) => (month === 1 ? prevYear - 1 : prevYear));
     };
 
-    fetchFinances();
-  }, [session, month, year]);
+    const handleNextMonth = () => {
+        setMonth((prevMonth) => (prevMonth === 12 ? 1 : prevMonth + 1));
+        setYear((prevYear) => (month === 12 ? prevYear + 1 : prevYear));
+    };
 
-  const handlePrevMonth = () => {
-    setMonth((prevMonth) => (prevMonth === 1 ? 12 : prevMonth - 1));
-    setYear((prevYear) => (month === 1 ? prevYear - 1 : prevYear));
-  };
+    return (
+        <div className="min-w-full">
+            <div className="flex items-center justify-center mb-3">
+                <MdOutlineNavigateBefore
+                    onClick={handlePrevMonth}
+                    className="text-primaryOrange cursor-pointer"
+                    size={38}
+                />
+                <h3 className="text-primaryOrange font-semibold text-2xl">{`${
+                    monthNames[month - 1]
+                }`}</h3>
+                <MdOutlineNavigateNext
+                    onClick={handleNextMonth}
+                    className="text-primaryOrange cursor-pointer"
+                    size={38}
+                />
+            </div>
 
-  const handleNextMonth = () => {
-    setMonth((prevMonth) => (prevMonth === 12 ? 1 : prevMonth + 1));
-    setYear((prevYear) => (month === 12 ? prevYear + 1 : prevYear));
-  };
-
-  return (
-    <div className="min-w-full">
-      <div className="flex items-center justify-center mb-3">
-        <MdOutlineNavigateBefore
-          onClick={handlePrevMonth}
-          className="text-primaryOrange cursor-pointer"
-          size={38}
-        />
-        <h3 className="text-primaryOrange font-semibold text-2xl">{`${
-          monthNames[month - 1]
-        }`}</h3>
-        <MdOutlineNavigateNext
-          onClick={handleNextMonth}
-          className="text-primaryOrange cursor-pointer"
-          size={38}
-        />
-      </div>
-
-      <div className="min-w-full rounded-2xl bg-primaryOrange p-0.5">
-        <table className="min-w-full bg-white overflow-hidden rounded-2xl">
-          <thead className="bg-primaryOrange rounded-t-lg">
-            <tr>
-              <th className="py-2 px-4 border-b text-white font-medium text-lg text-center">
-                Name
-              </th>
-              <th className="py-2 px-4 border-b text-white font-medium text-lg text-center">
-                Value
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {finances.map((finance, index) => (
-              <tr
-                key={finance.id}
-                className={index % 2 === 0 ? "bg-gray-100" : "bg-white"}
-              >
-                <td className="py-2 px-4 border-b border-primaryOrange text-constrastBlack font-medium text-center">
-                  {finance.name}
-                </td>
-                <td
-                  className={`py-2 px-4 border-b border-primaryOrange font-medium text-center ${
-                    finance.type === "INCOME"
-                      ? "text-green-500"
-                      : "text-red-500"
-                  }`}
-                >
-                  {new Intl.NumberFormat("pt-BR", {
-                    style: "currency",
-                    currency: "BRL",
-                  }).format(finance.value)}
-                </td>
-              </tr>
-            ))}
-            {finances.length === 0 && (
-              <tr>
-                <td
-                  colSpan={2}
-                  className="text-center text-constrastBlack font-medium text-lg"
-                >
-                  Suas finanças não chegaram aqui ainda...
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+            <div className="min-w-full rounded-2xl bg-primaryOrange p-0.5">
+                <table className="min-w-full bg-white overflow-hidden rounded-2xl">
+                    <thead className="bg-primaryOrange rounded-t-lg">
+                        <tr>
+                            <th className="py-2 px-4 border-b text-white font-medium text-lg text-center">
+                                Name
+                            </th>
+                            <th className="py-2 px-4 border-b text-white font-medium text-lg text-center">
+                                Valor
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {finances.map((finance, index) => (
+                            <tr
+                                key={finance.id}
+                                className={
+                                    index % 2 === 0 ? "bg-gray-100" : "bg-white"
+                                }
+                            >
+                                <td className="py-2 px-4 border-b border-primaryOrange text-constrastBlack font-medium text-center">
+                                    {finance.name}
+                                </td>
+                                <td
+                                    className={`py-2 px-4 border-b border-primaryOrange font-medium text-center ${
+                                        finance.type === "INCOME"
+                                            ? "text-green-500"
+                                            : "text-red-500"
+                                    }`}
+                                >
+                                    {new Intl.NumberFormat("pt-BR", {
+                                        style: "currency",
+                                        currency: "BRL",
+                                    }).format(finance.value)}
+                                </td>
+                            </tr>
+                        ))}
+                        {finances.length === 0 && (
+                            <tr>
+                                <td
+                                    colSpan={2}
+                                    className="text-center text-constrastBlack font-medium text-lg"
+                                >
+                                    Suas finanças não chegaram aqui ainda...
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
 };
 
 export default FinanceTable;
