773c4859e06d3008485bb66841c25104d191d8a6
Mateus Abreu <mateusabreucn@gmail.com>
2023-10-21 09:24:54 -0300

    :ok_hand: style: prettier style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,41 @@
-"use client";
+'use client'
 
-import Finances from "@/components/sections/finances/InsertFinances";
-import CardSection from "@/components/sections/homepage/CardSection";
-import CategoriesSection from "@/components/sections/homepage/CategoriesSection";
-import ChartsSection from "@/components/sections/homepage/ChartsSection";
-import FinanceTable from "@/components/sections/homepage/FinanceTable";
-import GoalsSection from "@/components/sections/homepage/GoalSection";
-import { useSession } from "next-auth/react";
+import Finances from '@/components/sections/finances/InsertFinances'
+import CardSection from '@/components/sections/homepage/CardSection'
+import CategoriesSection from '@/components/sections/homepage/CategoriesSection'
+import ChartsSection from '@/components/sections/homepage/ChartsSection'
+import FinanceTable from '@/components/sections/homepage/FinanceTable'
+import GoalsSection from '@/components/sections/homepage/GoalSection'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
-    const { data: session } = useSession();
+  const { data: session } = useSession()
+  const { push } = useRouter()
 
-    if (!session) {
-        return;
-    }
+  if (!session) {
+    push('/login')
+    return null;
+  }
 
-    return (
-        <main className="container flex min-h-screen flex-col items-center py-10 gap-y-12">
-            <div className="w-4/5 flex flex-col items-center gap-12">
-                <h1 className="text-primaryOrange font-semibold text-4xl self-start">
-                    Bem vindo(a), {session?.user?.name}
-                </h1>
-                <Finances />
-                <CardSection />
-                <FinanceTable />
-            </div>
-            <ChartsSection />
-            <div className="w-4/5 flex">
-                <GoalsSection />
-                <span className="border-2 border-constrastBlack bg-constrastBlack mx-auto my-20"></span>
-                <CategoriesSection />
-            </div>
-        </main>
-    );
+  return (
+    <main className="container flex min-h-screen flex-col items-center py-10 gap-y-12">
+      <div className="w-4/5 flex flex-col items-center gap-12">
+        <h1 className="text-primaryOrange font-semibold text-4xl self-start">
+          Bem vindo(a), {session?.user?.name}
+        </h1>
+        <Finances />
+        <CardSection />
+        <FinanceTable />
+      </div>
+
+      <ChartsSection />
+
+      <div className="w-4/5 flex">
+        <GoalsSection />
+        <span className="border-2 border-constrastBlack bg-constrastBlack mx-auto my-20"></span>
+        <CategoriesSection />
+      </div>
+    </main>
+  )
 }
