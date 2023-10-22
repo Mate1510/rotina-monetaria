a8f379ddd3a9fb6b2f81fdb69c6b6f3577271ff2
Mateus Abreu <mateusabreucn@gmail.com>
2023-10-22 09:42:41 -0300

    :test_tube: test: testing footer component

---
+++ src/components/sections/__tests__/Footer.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react'
+import Footer from '@/components/sections/Footer'
+
+jest.mock('next/link', () => {
+  const MockedNextLink = ({ children, href }: { children: any; href: any }) => (
+    <a href={href}>{children}</a>
+  )
+
+  MockedNextLink.displayName = 'MockedNextLink'
+
+  return MockedNextLink
+})
+
+describe('Footer', () => {
+  it('renders correctly', () => {
+    render(<Footer />)
+    expect(screen.getByAltText('Rotina Monetária')).toBeInTheDocument()
+  })
+
+  it('renders the correct links', () => {
+    render(<Footer />)
+
+    const feedbackLink = screen.getByRole('link', { name: /feedback/i })
+    expect(feedbackLink).toBeInTheDocument()
+    expect(feedbackLink).toHaveAttribute('href', 'https://www.google.com/')
+
+    const emailLink = screen.getByRole('link', { name: /email/i })
+    expect(emailLink).toBeInTheDocument()
+    expect(emailLink).toHaveAttribute('href', 'mailto:mateusabreucn@gmail.com')
+  })
+
+  it('renders the copyright text', () => {
+    render(<Footer />)
+    expect(screen.getByText('© 2023 Mateus Abreu')).toBeInTheDocument()
+  })
+})
