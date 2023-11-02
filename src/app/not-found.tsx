ce00eaaeeea4430368a1ffabfdb356d738de5d73
Mateus Abreu <mateusabreucn@gmail.com>
2023-11-02 19:39:42 -0300

    :sparkles: feat: page not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col w-min mx-auto gap-12 mt-32">
+      <h1 className="text-primaryOrange text-6xl text-center font-bold">
+        Erro 404
+      </h1>
+      <h1 className="text-primaryOrange text-4xl text-center font-semibold whitespace-nowrap ">
+        Página não encontrada =(
+      </h1>
+    </div>
+  )
+}
